906a9301b05c27b3f2d9f8d2d16605ba2fcc007d
jinheonyoon <yjinheon@gmail.com>
2025-01-16 10:27:02 +0900

    feat(misc): add desktop entry for linux idea

---
+++ misc/add_idea_desktop.sh
@@ -0,0 +1,15 @@
+# Add the following to /usr/share/applications/idea.desktop
+# /opt/idea/ and symlink to the current version with head/ -> /opt/idea/head/
+
+[Desktop Entry]
+Version=2025.1
+Name=Intellij Ultimate Edition
+Comment=The Drive to Develop
+GenericName=Intellij IDEA
+Exec=/opt/idea/head/bin/idea.sh
+Icon=/opt/idea/head/bin/idea.png
+Type=Application
+Terminal=false
+StartupNotify=true
+Categories=GNOME
+MimeType=x-scheme-handler/idea
